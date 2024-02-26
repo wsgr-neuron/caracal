8d170d91d70fffa21d10a7c6545e052f78cdde54
Nathan Ladd <nathanladd@gmail.com>
2024-02-26 16:10:54 -0600

    Tests are updated to use latest fixture

--- lib/caracal/fixtures.rb
+++
@@ -1,9 +0,0 @@
-require "initializer"
-
-require "test_bench/fixture"
-
-require "xml/fixtures"
-
-require "caracal/fixtures/docx/body"
-require "caracal/fixtures/docx/document"
-require "caracal/fixtures/docx/package"
